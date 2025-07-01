776a8a49e1495c99f546082ee42a2acdad460c68
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-07-01 20:18:57 +0300

    Integrate the template files needed for the project UI

--- cozzy/settings.py
+++ cozzy/settings.py
@@ -45,7 +45,9 @@ ROOT_URLCONF = 'cozzy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -101,11 +103,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
