43929ab665d8af71fc7d06633053c2be9db7eb78
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-07-06 01:39:58 +0300

    feat: Move SECRET_KEY to .env file

--- cozzy/settings.py
+++ cozzy/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4*bcuy$hn5s3+-t6l0utrwlr+_j#ko*hv2+$l+zr0wshf2@la'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
