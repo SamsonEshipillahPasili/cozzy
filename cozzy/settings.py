305907aa2ce3af28ada5ee8b9fc4164ec4b6bbe8
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2025-07-06 01:25:07 +0300

    UPdate the verification_sent template

--- cozzy/settings.py
+++ cozzy/settings.py
@@ -25,9 +25,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
